90e68237a8a884105871473e02c3aadae3ed5737
Neo Aliaga <josephneoar@gmail.com>
2024-04-10 17:48:11 -0400

    Fixes

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main extends JFrame {
     private static final String DB_PASSWORD = "Guitarhero3-*$.";
     
     public Main() {
-        setTitle("Sistema Facturación NEO");
+        setTitle("Iniciar Sesion");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         // Crear los componentes de la interfaz de usuario
